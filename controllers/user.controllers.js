4339a08c403367ac5dfd63d3d44570c14d8cdcde
Sharjeel Khalid <sharjeelac@gmail.com>
2025-02-10 05:27:24 +0500

    create user login router

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,29 +1,43 @@
 const userModel = require("../models/user-Model.js");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const generateToken = require('../utils/generateToken.js')
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const generateToken = require("../utils/generateToken.js");
 
 module.exports.userRegister = async (req, res) => {
-    try {
-      const { fullname, email, password } = req.body;
+  try {
+    const { fullname, email, password } = req.body;
 
-      const checkUser = await userModel.findOne({email : email})
-      if(checkUser) return res.status(401).send('Email have already an account')
-  
-      const salt = await bcrypt.genSalt(10)
-      const hash = await bcrypt.hash(password, salt)
-  
-      const newUser = await userModel.create({
-        fullname,
-        email,
-        password : hash,
-      });
-  
-      const token = generateToken(newUser)
+    const checkUser = await userModel.findOne({ email: email });
+    if (checkUser) return res.status(401).send("Email have already an account");
 
-      res.send('User Created')
-    } catch (error) {
-      res.send(error.message);
-    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const newUser = await userModel.create({
+      fullname,
+      email,
+      password: hash,
+    });
+
+    const token = generateToken(newUser);
+
+    res.send("User Created");
+  } catch (error) {
+    res.send(error.message);
   }
+};
+
+module.exports.userLogin = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  const checkuser = await userModel.findOne({ email: email });
+
+  if (!checkuser) return res.send("email or password is incorrect");
+
+  const checkPassword = await bcrypt.compare(password, checkuser.password);
+  if (!checkPassword) return res.send("email or password is incorrect");
+
+  const token = generateToken(checkuser)
+  res.cookie('token', token)
+
+  res.send("You are loggind");
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { userRegister } = require("../controllers/user.controllers.js");
+const { userRegister, userLogin } = require("../controllers/user.controllers.js");
 
 router.get("/test", (req, res) => {
   res.send("User Route Working");
 });
 
 router.post("/register", userRegister);
+router.post('/login', userLogin)
 
 module.exports = router;
